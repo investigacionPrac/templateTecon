c860e92efaa8da12107d44f901636715916f0961
AlvaroMartinezGuija <amartinez@tecon.es>
2025-08-18 09:54:39 +0200

    eliminar archivos innecesarios

--- .github/scripts/test.ps1
+++
@@ -1,6 +0,0 @@
-$environments = (gh api repos/investigacionPrac/InvPrac/environments) | ConvertFrom-Json
-$names = $environments.environments.Name
-
-foreach ($envName in $names){
-        gh workflow run "test2.yaml" --ref main --field entorno=$envName
-    }
